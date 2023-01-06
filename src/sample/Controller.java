1bbb2dc49c85f7ac2c5fde2f81b38fc688bcdbdc
ant <achavez@student.42.us.org>
2023-01-05 18:10:47 -0700

    init

---
+++ src/sample/Controller.java
@@ -0,0 +1,33 @@
+package sample;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private Button btn_submit;
+
+    @FXML
+    private Label label_banner;
+
+    @FXML
+    private Label label_msg;
+
+    @FXML
+    private Label label_password;
+
+    @FXML
+    private Label label_username;
+
+    @FXML
+    private Label label_zoneID;
+
+    @FXML
+    private TextField tf_password;
+
+    @FXML
+    private TextField tf_username;
+
+}
